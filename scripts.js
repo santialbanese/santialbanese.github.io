2b76edbc89759fb4986868c948069d43426d57f4
santialbanese <santialbanese11@gmail.com>
2024-08-28 15:18:27 -0300

    cambios3

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let lastScrollTop = 0;
-    const header = document.querySelector('.main-header');
-
-    window.addEventListener('scroll', function() {
-        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop > lastScrollTop) {
-            header.classList.add('hidden');
-        } else {
-            header.classList.remove('hidden');
-        }
-        lastScrollTop = scrollTop;
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const modeToggle = document.getElementById("mode-toggle");
     const currentMode = localStorage.getItem("theme");
