08ad2b5f50d31f2e002c9293fe44ee99155a7ad5
santialbanese <santialbanese11@gmail.com>
2024-08-29 00:42:38 -0300

    cambios3

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const modeToggle = document.getElementById("mode-toggle");
     const currentMode = localStorage.getItem("theme");
 
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.classList.add("dark-mode");
     }
 
-    modeToggle.addEventListener("click", function() {
+    modeToggle.addEventListener("click", function () {
         document.body.classList.toggle("dark-mode");
 
         let theme = "light";
@@ -16,3 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
         localStorage.setItem("theme", theme);
     });
 });
+
+
+var map = L.map('map').setView([-35.1000, -57.8945], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-34.9214, -57.9545]).addTo(map)
+    .bindPopup('La Plata');
